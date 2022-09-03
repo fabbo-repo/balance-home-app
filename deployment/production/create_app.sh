1c10097ad89aa22f56e1189bb2f0cf85510245d0
Fabián Scherle <fscherle98@gmail.com>
2022-09-03 13:43:10 +0200

    Docker-compose with letsencrypt (default only with nginx)

--- deployment/production/create_app.sh
+++ deployment/production/create_app.sh
@@ -9,6 +9,7 @@ cp ./extras_nginx.env ./balance_app/
 cp -r ./pg_data ./balance_app/
 cp -r ./nginx ./balance_app/
 
+cp ./docker-compose_letsencrypt.yml ./balance_app/
 cp ./docker-compose.yml ./balance_app/
 cp ./Dockerfile ./balance_app/
 cp -r ../../djangorest/* ./balance_app/
