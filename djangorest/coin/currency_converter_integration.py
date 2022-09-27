b1f425eb6b14bf85bb3fa44edd7d2c0ee906321d
Fabián Scherle <fscherle98@gmail.com>
2022-09-27 10:03:22 +0200

    Integration almos finished

--- djangorest/coin/currency_converter_integration.py
+++ djangorest/coin/currency_converter_integration.py
@@ -1,46 +1,71 @@
+import json
 import logging
 
 from datetime import timedelta
-
 from django.utils.timezone import now
+from coin.models import CoinExchange, CoinType
 
-from movies.models import Genre, SearchTerm, Movie
+from django.utils.translation import gettext_lazy as _
 
-from currency_converter.django_client import get_converter_from_db
+from currency_converter.django_client import get_converter_from_settings
 
 logger = logging.getLogger(__name__)
 
-def covert(coin_from, coin_to, amount):
-    # TODO
-    pass
 
-def search_and_save(search):
+class NoCoinExchangeException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = _('No coin exchange')
+        super().__init__(self.message)
+
+class OldExchangeException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = _('Exchange data not updated in the past 24 hours')
+        super().__init__(self.message)
+
+class UnsupportedExchangeException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = _('No exchange data for the given coin type')
+        super().__init__(self.message)
+
+
+def _convert(coin_from: CoinType, coin_to: CoinType, amount: float):
+    last_coin_exchange = CoinExchange.objects.last()
+    if not last_coin_exchange: raise NoCoinExchangeException()
+
+    # More than 24 hours:
+    if last_coin_exchange.created > now() - timedelta(days=1):
+        raise OldExchangeException()
+    
+    data = dict(json.loads(last_coin_exchange.exchange_data))
+    if not data or not coin_from.code in data.keys(): 
+        raise NoCoinExchangeException()
+
+    currency_converter = get_converter_from_settings()
+    currency_data = currency_converter.get_currency_data_from_json(
+        { coin_from.code : data[coin_from.code] }
+    )
+    return currency_data.convert(coin_from.code, coin_to.code, amount)
+
+
+def convert_or_fetch(coin_from: CoinType, coin_to: CoinType, amount: float):
     """
-    Perform a search for search_term against the API, but only
-    if it hasn't been searched in the past 24 hours. Save
-    each result to the local DB as a partial record.
+    Perform a conversion for two coins and an amount using an online 
+    converter. If the conversion has not been stored in db in 
+    the past 24 hours it will be fetched from the online converter
     """
-    # Replace multiple spaces with single spaces, and lowercase the search
-    normalized_search_term = re.sub(r"\s+", " ", search.lower())
-    search_term, created = SearchTerm.objects.get_or_create(term=normalized_search_term)
-    if not created and (search_term.last_search > now() - timedelta(days=1)):
-        # Don't search as it has been searched recently
-        logger.warning( "Search for '%s' was performed in the past 24 hours so not searching again.",
-            normalized_search_term,
-        )
-        return
-    omdb_client = get_client_from_settings()
-    for omdb_movie in omdb_client.search(search):
-        logger.info("Saving movie: '%s' / '%s'",
-            omdb_movie.title, omdb_movie.imdb_id)
-        
-        movie, created = Movie.objects.get_or_create(
-            imdb_id=omdb_movie.imdb_id,
-            defaults={
-                "title": omdb_movie.title,
-                "year": omdb_movie.year,
-            },
-        )
-        if created:
-            logger.info("Movie created: '%s'", movie.title)
-    search_term.save()
+    try:
+        return _convert(coin_from, coin_to, amount)
+    except Exception as e:
+        logger.error(str(e))
+        currency_converter = get_converter_from_settings()
+        return currency_converter.make_conversion(coin_from.code, coin_to.code, amount)
+
+def update_exchange_data():
+    """
+    Create a new CoinExchange for today
+    """
+    currency_converter = get_converter_from_settings()
+    CoinExchange.objects.create(
+        exchange_data = json.dumps(
+            dict(currency_converter.get_currency_data().data))
+    )
--- djangorest/currency_converter/converter.py
+++ djangorest/currency_converter/converter.py
@@ -7,39 +7,53 @@ logger = logging.getLogger(__name__)
 CURRENCY_CONVERTER_URL = "https://www.xe.com/currencyconverter/convert/"
 
 
-class CurrencyData:
+class ConnectionException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = 'Currency converter connection error'
+        super().__init__(self.message)
 
+
+class HtmlParserException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = 'Html parser no longer valid'
+        super().__init__(self.message)
+
+
+class CurrencyData:
     def __init__(self, data):
         """Data is a dict with the currency conversions"""
         self.data = data
 
-    def convert(self, currency_to, currency_from, ammount):
-        return self.data[currency_to][currency_from] * ammount
+    def convert(self, currency_from, currency_to, ammount: float):
+        return float(self.data[currency_from][currency_to]) * ammount
 
 
 class CurrencyConverterService:
     def __init__(self, currency_codes):
         self.currency_codes = currency_codes
 
-    def make_request(self, reference_currency_code, ammount=1):
+    def make_conversions(self, reference_currency_code, ammount=1):
         res = {
             reference_currency_code : ammount
         }
         for code in self.currency_codes:
             # Ignore reference_currency_code
             if code == reference_currency_code: continue
-            params = {}
-            params["Amount"] = ammount
-            params["From"] = reference_currency_code
-            params["To"] = code
-            resp = requests.get(CURRENCY_CONVERTER_URL, params=params)
-            try: resp.raise_for_status()
-            except: 
-                logger.error('[CURRENCY CONVERTER] currency converter connection error')
-                raise Exception('currency converter connection error')
-            html = resp.content
-            res[code] = self._filter_html_response(html)
+            res[code] = self.make_conversion(reference_currency_code, code, ammount)
         return res
+    
+    def make_conversion(self, code_from, code_to, ammount=1):
+        params = {}
+        params["Amount"] = ammount
+        params["From"] = code_from
+        params["To"] = code_to
+        resp = requests.get(CURRENCY_CONVERTER_URL, params=params)
+        try: resp.raise_for_status()
+        except: 
+            logger.error('[CURRENCY CONVERTER] currency converter connection error')
+            raise ConnectionError()
+        html = resp.content
+        return self._filter_html_response(html)
     
     def _filter_html_response(self, html):
         soup = BeautifulSoup(html, 'html.parser')
@@ -51,12 +65,12 @@ class CurrencyConverterService:
     
     def get_currency_data(self):
         logger.info("[CURRENCY CONVERTER] Fetching currency data")
-        
         res = {}
-        
         for code in self.currency_codes:
-            data = self.make_request(code)
+            data = self.make_conversions(code)
             data.pop(code)
             res[code] = data
-
         return CurrencyData(res)
+    
+    def get_currency_data_from_json(self, data):
+        return CurrencyData(data)
