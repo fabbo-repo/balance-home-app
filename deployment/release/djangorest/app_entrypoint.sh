57f71b42bb5507acc4f98067b29ce48a15d7b0f9
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2022-10-07 22:38:11 +0200

    Changes to deployment process

--- deployment/release/djangorest/app_entrypoint.sh
+++ deployment/release/djangorest/app_entrypoint.sh
@@ -16,28 +16,28 @@ touch /var/log/balance_app/app.log
 chmod 777 /var/log/balance_app/app.log
 
 # Call collectstatic
-/venv/bin/python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Execute database migrations
-if  /venv/bin/python manage.py migrate --check; then
+if  python3 manage.py migrate --check; then
     echo Migrations applied
 else
     echo Applying migrations
-    /venv/bin/python manage.py migrate --noinput
+    python3 manage.py migrate --noinput
 fi
 
 # Create superuser
-if  /venv/bin/python manage.py createsuperuser --no-input; then
+if  python3 manage.py createsuperuser --no-input; then
     echo Superuser created
 else
     echo Superuser already created, skipping
 fi
 
 # App commands
-/venv/bin/python manage.py balance_schedule_setup
-/venv/bin/python manage.py create_balance_models
-/venv/bin/python manage.py coin_schedule_setup
-/venv/bin/python manage.py create_coin_models
+python3 manage.py balance_schedule_setup
+python3 manage.py create_balance_models
+python3 manage.py coin_schedule_setup
+python3 manage.py create_coin_models
 
 cat << "EOF"
 
@@ -56,4 +56,4 @@ B)bbbbb   a)AAAA l)LL  a)AAAA n)   NN  c)CCCC  e)EEEE    H)    hh  o)OOO  m)    
 EOF
 
 APP_USER_UID=`id -u $APP_USER`
-exec /venv/bin/uwsgi --uid=$APP_USER_UID --http-auto-chunked --http-keepalive  "$@"
+exec uwsgi --uid=$APP_USER_UID --http-auto-chunked --http-keepalive --ini app.uwsgi.ini "$@"
