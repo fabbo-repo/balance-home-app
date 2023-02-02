142ec7865f208889fdbd9bd83a06487d6969903c
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-02-02 15:59:19 +0100

    Update http service and remove imports

--- frontend/flutter/lib/src/features/statistics/infrastructure/repositories/monthly_balance_repository.dart
+++ frontend/flutter/lib/src/features/statistics/infrastructure/repositories/monthly_balance_repository.dart
@@ -1,8 +1,5 @@
-import 'package:balance_home_app/config/api_contract.dart';
-import 'package:balance_home_app/src/core/domain/entities/pagination_entity.dart';
 import 'package:balance_home_app/src/core/domain/failures/failure.dart';
 import 'package:balance_home_app/src/features/statistics/infrastructure/datasources/remote/monthly_balance_remote_data_source.dart';
-import 'package:balance_home_app/src/http_service.dart';
 import 'package:balance_home_app/src/features/statistics/domain/entities/monthly_balance_entity.dart';
 import 'package:balance_home_app/src/features/statistics/domain/repositories/monthly_balance_repository_interface.dart';
 import 'package:fpdart/fpdart.dart';
