d4065b7c6b835584b13d668720407fc724cc9917
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-02-10 22:50:24 +0100

    Update views.py

--- backend/djangorest/src/frontend_version/api/views.py
+++ backend/djangorest/src/frontend_version/api/views.py
@@ -26,6 +26,6 @@ class FrontendVersionView(APIView):
         except:
             return Response(
                 data= {
-                    'version': "1.0.0"
+                    'version': "1.0.4"
                 }
             )
