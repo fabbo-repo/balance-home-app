e8243d062a3cd4c8738a631870210693a9075828
fbsrvuser <fbsrvuser@UbuntuMateDevelop.myguest.virtualbox.org>
2023-04-01 14:29:25 +0200

    minio setup for django

--- backend-djangorest/djangorest/src/core/settings.py
+++ backend-djangorest/djangorest/src/core/settings.py
@@ -32,6 +32,16 @@ env = environ.Env(
         "APP_EMAIL_HOST_PASSWORD", default='password')),
     APP_CELERY_BROKER_URL=(str, os.getenv(
         "APP_CELERY_BROKER_URL", default="redis://localhost:6379/0")),
+    APP_MINIO_HOST=(str, os.getenv(
+        "APP_MINIO_HOST", default="localhost")),
+    APP_MINIO_ACCESS_KEY=(str, os.getenv(
+        "APP_MINIO_ACCESS_KEY", default="")),
+    APP_MINIO_SECRET_KEY=(str, os.getenv(
+        "APP_MINIO_SECRET_KEY", default="")),
+    APP_MINIO_BUCKET_NAME=(str, os.getenv(
+        "APP_MINIO_MEDIA_BUCKET_NAME", default="")),
+    APP_MINIO_BUCKET_NAME=(str, os.getenv(
+        "APP_MINIO_STATIC_BUCKET_NAME", default="")),
 )
 
 
@@ -91,6 +101,8 @@ class Dev(Configuration):
         'django_celery_beat',
         # Backup
         'dbbackup',
+        # Minio
+        'minio_storage',
         # Custom apps:
         'core',
         'custom_auth',
@@ -355,3 +367,16 @@ class OnPremise(Dev):
     EMAIL_PORT = env('APP_EMAIL_PORT')
     EMAIL_HOST_USER = env('APP_EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = env('APP_EMAIL_HOST_PASSWORD')
+
+    DEFAULT_FILE_STORAGE = 'minio_storage.storage.MinioMediaStorage'
+    STATICFILES_STORAGE = 'minio_storage.storage.MinioMediaStorage'
+
+    MINIO_STORAGE_ENDPOINT = env('APP_EMAIL_HOST_PASSWORD')
+    MINIO_STORAGE_ACCESS_KEY = env('APP_MINIO_ACCESS_KEY')
+    MINIO_STORAGE_SECRET_KEY = env('APP_MINIO_SECRET_KEY')
+    MINIO_STORAGE_MEDIA_BUCKET_NAME = env('APP_MINIO_MEDIA_BUCKET_NAME')
+    MINIO_STORAGE_STATIC_BUCKET_NAME = env('APP_MINIO_STATIC_BUCKET_NAME')
+    MINIO_STORAGE_MEIDA_USE_PRESIGNED = True
+    MINIO_STORAGE_MEIDA_URL_EXPIRY = 3600
+    MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
+    MINIO_STORAGE_AUTO_CREATE_STATIC_BUCKET = True
