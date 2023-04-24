28a7fd3a8dbf4cd2f2b6b03bb0e257bbe5023766
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-04-24 12:49:19 +0200

    Update collectmedia.py

--- backend-djangorest/djangorest/src/core/management/commands/collectmedia.py
+++ backend-djangorest/djangorest/src/core/management/commands/collectmedia.py
@@ -3,7 +3,6 @@ from django.conf import settings
 import os
 import logging
 from minio import Minio
-import mimetypes
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +14,7 @@ class Command(BaseCommand):
     python manage.py collectmedia
     ~~~
     """
+
     help = "Upload default stored media files to minio bucket"
 
     def handle(self, *args, **options):
@@ -22,21 +22,20 @@ class Command(BaseCommand):
             endpoint=settings.MINIO_STORAGE_ENDPOINT,
             access_key=settings.MINIO_STORAGE_ACCESS_KEY,
             secret_key=settings.MINIO_STORAGE_SECRET_KEY,
-            secure=True
+            secure=True,
         )
-        for (dirpath, _, filenames) in os.walk(settings.MEDIA_ROOT):
+        for dirpath, _, filenames in os.walk(settings.MEDIA_ROOT):
             for file in filenames:
-                if str(file).lower().endswith(('.png', '.jpg', '.jpeg')):
+                if str(file).lower().endswith((".png", ".jpg", ".jpeg")):
                     filepath = os.path.join(dirpath, file)
+                    object_name = str(filepath).removeprefix(
+                        os.path.join(str(settings.MEDIA_ROOT), ""))
+                    print("Uploading {}".format(object_name))
                     try:
-                        with open(filepath, 'rb') as image_data:
-                            file_stat = os.stat(filepath)
-                            minio_client.put_object(
-                                bucket_name=settings.MINIO_STORAGE_MEDIA_BUCKET_NAME,
-                                object_name=file,
-                                data=image_data,
-                                length=file_stat.st_size,
-                                content_type=mimetypes.guess_type(filepath)
-                            )
+                        minio_client.fput_object(
+                            bucket_name=settings.MINIO_STORAGE_MEDIA_BUCKET_NAME,
+                            object_name=object_name,
+                            file_path=filepath,
+                        )
                     except Exception as err:
-                        logger.error(str(err))
+                        print(err)
