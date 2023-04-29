38de6cdc60f24192c0417386a118aafd732a56ab
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-04-29 13:01:03 +0200

    Create failure_utils.dart

---
+++ frontend-flutter/flutter/lib/src/core/utils/failure_utils.dart
@@ -0,0 +1,21 @@
+
+import 'package:balance_home_app/src/core/domain/failures/bad_request_failure.dart';
+import 'package:balance_home_app/src/core/domain/failures/failure.dart';
+import 'package:balance_home_app/src/core/domain/failures/http_request_failure.dart';
+import 'package:balance_home_app/src/core/domain/failures/unauthorized_request_failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+class FailureUtils {
+  static Either<Failure, Map<String, dynamic>> checkResponse({required Map<String, dynamic> body, required int statusCode}) {
+    if (statusCode/10 == 20) {
+      return right(body);
+    }
+    else if (statusCode == 400) {
+      return left(BadRequestFailure.fromJson(body));
+    }
+    else if (statusCode == 401) {
+      return left(UnauthorizedRequestFailure.fromJson(body));
+    }
+    return left(HttpRequestFailure(detail: '', statusCode: statusCode));
+  }
+}
