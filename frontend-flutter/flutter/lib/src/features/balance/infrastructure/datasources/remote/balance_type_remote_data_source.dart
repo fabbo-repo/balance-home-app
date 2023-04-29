678da568b56f9586de12b1c18db71d7bf00ab4e2
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-04-29 13:01:48 +0200

    datasource fixes

--- frontend-flutter/flutter/lib/src/features/balance/infrastructure/datasources/remote/balance_type_remote_data_source.dart
+++ frontend-flutter/flutter/lib/src/features/balance/infrastructure/datasources/remote/balance_type_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:balance_home_app/src/core/domain/entities/pagination_entity.dart';
 import 'package:balance_home_app/config/api_contract.dart';
+import 'package:balance_home_app/src/core/domain/failures/empty_failure.dart';
 import 'package:balance_home_app/src/core/domain/failures/failure.dart';
+import 'package:balance_home_app/src/core/utils/failure_utils.dart';
 import 'package:balance_home_app/src/http_client.dart';
 import 'package:balance_home_app/src/features/balance/domain/entities/balance_type_entity.dart';
 import 'package:balance_home_app/src/features/balance/domain/repositories/balance_type_mode.dart';
@@ -17,10 +19,11 @@ class BalanceTypeRemoteDataSource {
         ? APIContract.expenseType
         : APIContract.revenueType;
     HttpResponse response = await client.sendGetRequest('$baseUrl/$name');
-    if (response.hasError) {
-      return left(Failure.badRequest(message: response.errorMessage));
-    }
-    return right(BalanceTypeEntity.fromJson(response.content));
+    // Check if there is a request failure
+    final responseCheck = FailureUtils.checkResponse(
+        body: response.content, statusCode: response.statusCode);
+    return responseCheck.fold((failure) => left(failure),
+        (body) => right(BalanceTypeEntity.fromJson(body)));
   }
 
   Future<Either<Failure, List<BalanceTypeEntity>>> list(
@@ -31,8 +34,12 @@ class BalanceTypeRemoteDataSource {
     int pageNumber = 1;
     HttpResponse response =
         await client.sendGetRequest('$baseUrl?page=$pageNumber');
-    if (response.hasError) {
-      return left(Failure.badRequest(message: response.errorMessage));
+    // Check if there is a request failure
+    final responseCheck = FailureUtils.checkResponse(
+        body: response.content, statusCode: response.statusCode);
+    if (responseCheck.isLeft()) {
+      return left(
+          responseCheck.getLeft().getOrElse(() => const EmptyFailure()));
     }
     PaginationEntity page = PaginationEntity.fromJson(response.content);
     List<BalanceTypeEntity> balanceTypes =
@@ -41,8 +48,12 @@ class BalanceTypeRemoteDataSource {
       pageNumber++;
       HttpResponse response =
           await client.sendGetRequest('$baseUrl?page=$pageNumber');
-      if (response.hasError) {
-        return left(Failure.badRequest(message: response.errorMessage));
+      // Check if there is a request failure
+      final responseCheck = FailureUtils.checkResponse(
+          body: response.content, statusCode: response.statusCode);
+      if (responseCheck.isLeft()) {
+        return left(
+            responseCheck.getLeft().getOrElse(() => const EmptyFailure()));
       }
       page = PaginationEntity.fromJson(response.content);
       balanceTypes +=
