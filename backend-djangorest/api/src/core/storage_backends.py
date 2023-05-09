b3b7ea7b10ad570807e14693950784d20158285d
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-05-09 19:45:37 +0200

    storage backends

---
+++ backend-djangorest/api/src/core/storage_backends.py
@@ -0,0 +1,14 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+
+class MinioStaticStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'private'
+    bucket_name = settings.APP_MINIO_STATIC_BUCKET_NAME
+
+
+class MinioMediaStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
+    bucket_name = settings.APP_MINIO_MEDIA_BUCKET_NAME
