d9225c06076a1f9c1acf4be0a0c850ed8ab2356d
fabbo-repo <fscherle98@gmail.com>
2023-08-27 19:55:59 +0200

    Keycloak env variables

--- frontend-flutter/flutter/lib/config/environment.dart
+++ frontend-flutter/flutter/lib/config/environment.dart
@@ -5,7 +5,19 @@ part 'environment.g.dart';
 // This class contains all the environment variables
 @Envied(path: 'app.env', requireEnvFile: true)
 abstract class Environment {
-  /// URL for API
+  /// Balhom API URL
   @EnviedField(varName: "API_URL", defaultValue: "localhost")
   static const String apiUrl = _Environment.apiUrl;
+  
+  /// Keycloak URL
+  @EnviedField(varName: "KEYCLOAK_URL", defaultValue: "localhost")
+  static const String keycloakUrl = _Environment.keycloakUrl;
+
+  /// Keycloak Realm
+  @EnviedField(varName: "KEYCLOAK_REALM", defaultValue: "balhom")
+  static const String keycloakRealm = _Environment.keycloakRealm;
+
+  /// Keycloak Client Id
+  @EnviedField(varName: "KEYCLOAK_CLIENT_ID", defaultValue: "balhom")
+  static const String keycloakClientId = _Environment.keycloakClientId;
 }
